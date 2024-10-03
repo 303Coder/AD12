02d6b4d67d220d0b96f0517a6d28304f683cbbb2
YeshUpadhyaya <72773359+YeshUpadhyaya@users.noreply.github.com>
2024-10-02 21:18:47 -0600

    Update UItest.py

--- UItest.py
+++ UItest.py
@@ -1,16 +1,18 @@
 import tkinter as tk
 from tkinter import filedialog
-
+from asin import reader
+x='t'
 def upload_file():
     file_path = filedialog.askopenfilename()
     if file_path:
-        print('pressed')
+        x=reader.getAmount(file_path)
+        lbl = tk.Label(DonApp, text=x)
+        lbl.grid()
 
 DonApp = tk.Tk()
 DonApp.title("Total Donation Count")
-lbl = tk.Label(DonApp, text="Donation count")
-lbl
+
 button = tk.Button(DonApp, text="Upload File", command=upload_file)
-button.pack(expand=1)
+button.grid()
 
 DonApp.mainloop()
