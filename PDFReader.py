c00b02eb4f8ed89897ed11709a164be42a59f93c
DevinShea <dshe245806@adams12.org>
2024-10-09 12:26:34 -0700

    Update PDFReader.py

--- PDFReader.py
+++ PDFReader.py
@@ -8,21 +8,20 @@ import pdfplumber
 # Replace '' with the path to your PDF
 pdf_path = 'C:\\Users\\BTEC_Cyber_10\\Desktop\\Python VS\\.venv\\Inventory.pdf'
 
-first_column_list = []
-last_column_list = []
+ASIN_list = []
+Amount_list = []
 
 with pdfplumber.open(pdf_path) as pdf:
     for page in pdf.pages:
         tables = page.extract_tables()
 
         for table in tables:
-            # Start iterating from the third row (index 2)
-            for row in table[2:]:
+            for row in table[2:]:   #Start iterating from the third row (index 2)
                 if row:
                     first_column = row[0]
                     last_column = row[-1]
-                    first_column_list.append(first_column)
-                    last_column_list.append(last_column)
+                    ASIN_list.append(first_column)
+                    Amount_list.append(last_column)
 
-print(first_column_list)
-print(last_column_list)
+print(ASIN_list)
+print(Amount_list)
